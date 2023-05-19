2bf9872155ef15d56d6f72142f1cf3524fbe68c7
Jaarabytes <128234882+Jaarabytes@users.noreply.github.com>
2023-05-19 22:55:57 +0300

    used the env file

--- myApp.js
+++ myApp.js
@@ -1,3 +1,5 @@
+require('dotenv').config() 
+
 let express = require('express');
 let app = express();
 console.log("Hello World");
@@ -5,12 +7,18 @@ console.log("Hello World");
 app.get("/views/index.html", function(req,res){
     res.send("Hello Express");
 });
-app.use("/public" , express.static("__dirname + /public"))
+app.use("/public" , express.static(__dirname + "/public"))
 
-
-
-
-
+app.get("/json", function(res,req){
+    res.send({"message": "Hello json"})
+});
+app.use("your-app-url/json", express.static(__dirname+"your-app-url/json"));
+var response = "Hello World".toUpperCase();
+if (process.env.VAR_NAME === "allCaps") {
+    response = "Hello World".toUpperCase();
+  } else {
+    response = "Hello World";
+  }
 
 
 
