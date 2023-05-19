086cb41ba9af68c49e8c7764641f4eca65bcc02e
Jaarabytes <128234882+Jaarabytes@users.noreply.github.com>
2023-05-19 22:39:22 +0300

    served the json file

--- myApp.js
+++ myApp.js
@@ -4,7 +4,8 @@ console.log("Hello World");
 
 app.get("/views/index.html", function(req,res){
     res.send("Hello Express");
-})
+});
+app.use("/public" , express.static("__dirname + /public"))
 
 
 
